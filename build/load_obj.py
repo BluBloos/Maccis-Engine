223c499bafd0e5a9f8791d966840c9fc020395b6
BluBloos <noah.cabral.104@my.aquinas.me>
2018-08-02 13:35:13 -0400

    created new branch for testing out the python obj loader module

--- build/load_obj.py
+++ build/load_obj.py
@@ -1,15 +1,36 @@
 import sys
 
+class vertex:
+    def __init__(self, position, textureCoord, normal):
+        self.position = position
+        self.textureCoord = textureCoord
+        self.normal = normal
+    def compare(self, v):
+        if self.position.compare(v.position) and self.textureCoord.compare(v.textureCoord) and self.normal.compare(v.normal):
+            return True
+        return False
+
 class vec3:
     def __init__(self, x, y, z):
         self.x = x
         self.y = y
         self.z = z
+    def compare(self, v):
+        if self.x == v.x and self.y == v.y and self.z == v.z:
+          return True
+        return False
 
 class vec2:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+    def compare(self, v):
+        if self.x == v.x and self.y == v.y and self.z == v.z:
+          return True
+        return False
+
+def queryVertices(vertices, vertex):
+  pass
 
 def obj(fileName):
     sys.stdout = open("plog.txt", "w")
@@ -22,6 +43,9 @@ def obj(fileName):
     vertices = []
     textureCoords = []
     normals = []
+
+    vertexObjects = []
+
     floatBuffer = []
     indices = []
     vertexCount = 0
@@ -39,26 +63,32 @@ def obj(fileName):
         elif splt[0] == 'f':
             for i in range(3):
                 vertex = splt[i + 1].split("/")
-                indices.append(vertexCount)
-                vertexCount += 1
 
-                #append position of vertex 1
-                floatBuffer.append(vertices[int(vertex[0]) - 1].x)
-                floatBuffer.append(vertices[int(vertex[0]) - 1].y)
-                floatBuffer.append(vertices[int(vertex[0]) - 1].z)
+                tempTextureCoord = vec2(0, 0)
+                if vertex[1] != '':
+                    tempTextureCoord[0] = (textureCoords[int(vertex[1]) - 1].x)
+                    tempTextureCoord[1] = (textureCoords[int(vertex[1]) - 1].y)
 
-                #append texture coordinates of vertex 1
-                if vertex[1] == '':
-                    floatBuffer.append(0)
-                    floatBuffer.append(0)
-                else:
-                    floatBuffer.append(textureCoords[int(vertex[1]) - 1].x)
-                    floatBuffer.append(textureCoords[int(vertex[1]) - 1].y)
+                vertexObject = vertex( vertices[int(vertex[0]) - 1], tempTextureCoord,  normals[int(vertex[2]) - 1])
+                queryResult = queryVertices(vertexObjects, vertexObject)
+                if queryResult.good:
+                    #below appends a vertex to the floatBuffer
+                    #append position of vertex 1
+                    floatBuffer.append(vertices[int(vertex[0]) - 1].x)
+                    floatBuffer.append(vertices[int(vertex[0]) - 1].y)
+                    floatBuffer.append(vertices[int(vertex[0]) - 1].z)
 
-                #append normals of vertex 1
-                floatBuffer.append(normals[int(vertex[2]) - 1].x)
-                floatBuffer.append(normals[int(vertex[2]) - 1].y)
-                floatBuffer.append(normals[int(vertex[2]) - 1].z)
+                    #append texture coordinates of vertex 1
+                    floatBuffer.append(tempTextureCoord.x)
+                    floatBuffer.append(tempTextureCoord.y)
+
+                    #append normals of vertex 1
+                    floatBuffer.append(normals[int(vertex[2]) - 1].x)
+                    floatBuffer.append(normals[int(vertex[2]) - 1].y)
+                    floatBuffer.append(normals[int(vertex[2]) - 1].z)
+
+                indices.append(queryResult) #the query result will tell us the index of the requested vertex
+                vertexCount += 1 #add one to the vertexCount because we added more vertices
 
     for index in indices:
         floatBuffer.append(index)
@@ -67,4 +97,4 @@ def obj(fileName):
 
     print(floatBuffer)
     sys.stdout.close()
-    return floatBuffer
+    return [floatBuffer, indices]
