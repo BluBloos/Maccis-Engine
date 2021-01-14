ebd2649378ad0913d6efd24a2cb8f676fbd99f8d
Noah Cabral <38915815+BluBloos@users.noreply.github.com>
2021-01-14 18:49:14 -0500

    Update shell.bat

--- shell.bat
+++ shell.bat
@@ -1,4 +1,2 @@
 @echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
-set path=C:\dev\Maccis-Engine\bin\Windows\x86;%path%
-cd C:\dev\Maccis-Engine
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
